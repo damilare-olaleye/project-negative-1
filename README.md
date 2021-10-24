1991962867827a1d7c6cdf05e3960cc85c7a6373
Damilare Olaleye <damilare.olaleye@cyatmr.com>
2021-10-23 21:34:23 -0500

    complete project-negative-1     - add README input

--- README.md
+++ README.md
@@ -1,1 +1,55 @@
 # project-negative-1
+
+# Overview
+Your team at company X is currently working on a fullstack application that helps users perform basic arithmetic. They can visit the website, input two numbers, and perform a binary arithmetic operation (addition, subtraction, multiplication, division) on those numbers. They can then view the result of the calculation.
+
+
+## Manual Testing
+In order to see if the application is working as intended, you currently follow a manual process of sitting down at the computer, typing in two numbers on the keyboard, selecting the operation you would like to perform, and clicking the calculate button. If the output matches what you would expect if you did those calculations on paper, then you know that the application is working.
+
+Currently, you have to test the application manually, testing each operation (addition, subtraction, multiplication, division) with a variety of different combinations of numbers each and every time a new feature/change is made in the application.
+
+**Question:** Why do you need to test the application each time you make a change to it?
+> To ensure that new functionality / code you have added to the application did not cause old functionality to break
+
+Formally, this is known as **regression testing**. You a testing to make sure the application did not **regress**. Regress means to *go backwards*, which is not good.
+
+# The Project
+You have been tasked to automate this tedious process of testing out each operation (addition, subtraction, multiplication, division).
+
+To accomplish the task of automating these tasks, you will utilize the following technologies:
+- Java 8
+- Selenium WebDriver
+
+## Steps
+1. First, 
+    - You will have to make sure you have the calculator app up and running
+    - It should have the appropriate functionality to support
+        - Addition (already implemented during lecture)
+        - Subtraction
+        - Multiplication
+        - Division
+    - This will require 
+        - 4 different forms in your .HTML file to support each operation
+        - The necessary logic in your Java program
+            - 4 endpoint mappings (/add, /subtract, /multiply, /divide)
+            - Service layer methods
+
+2. Secondly,
+    1. Create a new project
+    2. Create a package that will house your main class
+    3. Write a script within the main class' main method that will
+        - Input 2 numbers, perform the add functionality, and then print out the result of the addition
+        - Input 2 numbers, perform the subtract functionality, and then print out the result of the subtraction
+        - Input 2 numbers, perform the multiply functionality, and then print out the result of the multiplication
+        - Input 2 numbers, perform the divide functionality, and then print out the result of the division
+
+# Deliverables
+- Push the automation project to Github and provide a link by the due date to me
+- Present your project on the due date
+    - 5 minute limit
+    - This project is relatively simple, so the presentation will likely not take long
+    - Run your code and demonstrate it working
+
+# Due Date
+- Wednesday (October 27th)
--- selenium-test/src/main/java/com/revature/main/Main.java
+++ selenium-test/src/main/java/com/revature/main/Main.java
@@ -25,16 +25,6 @@ public class Main {
 		addInput2.sendKeys("43.9");
 		addButton.click();
 
-		// pause
-		Thread.sleep(1000);
-		// this switch inside the add result iframe
-		driver.switchTo().frame("addResult");
-
-		WebElement addOutput = driver.findElement(By.tagName("pre"));
-		System.out.println("The result of adding 50.4 and 43.9 is: " + addOutput.getText());
-
-		// Locate the 2 input elements and button for subtracting numbers
-
 		WebElement subtractInput1 = driver.findElement(By.id("subtractNum1"));
 		WebElement subtractInput2 = driver.findElement(By.id("subtractNum2"));
 		WebElement subtractButton = driver.findElement(By.id("subtractButton"));
@@ -43,65 +33,32 @@ public class Main {
 		subtractInput2.sendKeys("43.9");
 		subtractButton.click();
 
-		// pause
-		Thread.sleep(1000);
-		// this switch inside the subtract result iframe
-		driver.switchTo().frame("subtractResult");
-
-		WebElement subtractOutput = driver.findElement(By.tagName("pre"));
-		System.out.println("The result of adding 50.4 and 43.9 is: " + subtractOutput.getText());
-
 		// Locate the 2 input elements and button for dividing numbers
 		WebElement divideInput1 = driver.findElement(By.id("divideNum1"));
 		WebElement divideInput2 = driver.findElement(By.id("divideNum2"));
 		WebElement divideButton = driver.findElement(By.id("divideButton"));
 
-		divideInput1.sendKeys("50.4");
-		divideInput2.sendKeys("43.9");
+		divideInput1.sendKeys("50.0");
+		divideInput2.sendKeys("10.0");
 		divideButton.click();
 
-		// pause
-		Thread.sleep(1000);
-		// this switch inside the divide result iframe
-		driver.switchTo().frame("subtractResult");
-
-		WebElement divideOutput = driver.findElement(By.tagName("pre"));
-		System.out.println("The result of adding 50.4 and 43.9 is: " + divideOutput.getText());
 
 		// Locate the 2 input elements and button for multiplying numbers
 		WebElement multiplyInput1 = driver.findElement(By.id("multiplyNum1"));
 		WebElement multiplyInput2 = driver.findElement(By.id("multiplyNum2"));
 		WebElement multiplyButton = driver.findElement(By.id("multiplyButton"));
 
-		multiplyInput1.sendKeys("50.4");
-		multiplyInput2.sendKeys("43.9");
+		multiplyInput1.sendKeys("5");
+		multiplyInput2.sendKeys("10");
 		multiplyButton.click();
 
-		// pause
-		Thread.sleep(1000);
-		// this switch inside the multiply result iframe
-		driver.switchTo().frame("multiplyResult");
-
-		WebElement multiplyOutput = driver.findElement(By.tagName("pre"));
-		System.out.println("The result of adding 50.4 and 43.9 is: " + multiplyOutput.getText());
-
-		// Locate the 2 input elements and button for mod numbers
 		WebElement modInput1 = driver.findElement(By.id("modNum1"));
 		WebElement modInput2 = driver.findElement(By.id("modNum2"));
 		WebElement modButton = driver.findElement(By.id("modButton"));
 
-		modInput1.sendKeys("50.4");
-		modInput2.sendKeys("43.9");
+		modInput1.sendKeys("12");
+		modInput2.sendKeys("7");
 		modButton.click();
-
-		// pause
-		Thread.sleep(1000);
-		
-		// this switch inside the mod result iframe
-		driver.switchTo().frame("multiplyResult");
-
-		WebElement modOutput = driver.findElement(By.tagName("pre"));
-		System.out.println("The result of adding 50.4 and 43.9 is: " + modOutput.getText());
 
 	}
 
