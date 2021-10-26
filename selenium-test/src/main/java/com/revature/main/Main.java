34943c339c109f4e50458f634a53de6d3f641d83
Damilare Olaleye <damilare.olaleye@cyatmr.com>
2021-10-26 10:23:00 -0500

    Print to console     - add iframe to print to console

--- selenium-test/src/main/java/com/revature/main/Main.java
+++ selenium-test/src/main/java/com/revature/main/Main.java
@@ -17,48 +17,85 @@ public class Main {
 		Thread.sleep(1000);
 
 		// Locate the 2 input elements and button for adding numbers
+		driver.switchTo().defaultContent();
+		
 		WebElement addInput1 = driver.findElement(By.id("addNum1"));
 		WebElement addInput2 = driver.findElement(By.id("addNum2"));
 		WebElement addButton = driver.findElement(By.id("addButton"));
-
+		
 		addInput1.sendKeys("50.4");
 		addInput2.sendKeys("43.9");
 		addButton.click();
-
+		driver.switchTo().frame("addResult");
+		
+		WebElement addOutput = driver.findElement(By.tagName("pre"));
+		String addFrameText = addOutput.getText();
+		System.out.println("Add frame prints: " + addFrameText);
+		driver.switchTo().defaultContent();
+		
+		// Subtract Elements
 		WebElement subtractInput1 = driver.findElement(By.id("subtractNum1"));
 		WebElement subtractInput2 = driver.findElement(By.id("subtractNum2"));
 		WebElement subtractButton = driver.findElement(By.id("subtractButton"));
 
-		subtractInput1.sendKeys("50.4");
+		subtractInput1.sendKeys("60.4");
 		subtractInput2.sendKeys("43.9");
 		subtractButton.click();
+		driver.switchTo().frame("subtractResult");
+
+		WebElement subtractOutput = driver.findElement(By.tagName("pre"));
+		String subtractFrameText = subtractOutput.getText();
+		System.out.println("Subtract frame prints: " + subtractFrameText);
+		driver.switchTo().defaultContent();
 
 		// Locate the 2 input elements and button for dividing numbers
 		WebElement divideInput1 = driver.findElement(By.id("divideNum1"));
 		WebElement divideInput2 = driver.findElement(By.id("divideNum2"));
 		WebElement divideButton = driver.findElement(By.id("divideButton"));
-
+		
 		divideInput1.sendKeys("50.0");
 		divideInput2.sendKeys("10.0");
 		divideButton.click();
+		driver.switchTo().frame("divideResult");
 
+		WebElement divideOutput = driver.findElement(By.tagName("pre"));
+		String divideFrameText = divideOutput.getText();
+		System.out.println("Divide frame prints: " + divideFrameText);
+		driver.switchTo().defaultContent();
+		
 
 		// Locate the 2 input elements and button for multiplying numbers
 		WebElement multiplyInput1 = driver.findElement(By.id("multiplyNum1"));
 		WebElement multiplyInput2 = driver.findElement(By.id("multiplyNum2"));
 		WebElement multiplyButton = driver.findElement(By.id("multiplyButton"));
-
-		multiplyInput1.sendKeys("5");
-		multiplyInput2.sendKeys("10");
+		
+		multiplyInput1.sendKeys("67");
+		multiplyInput2.sendKeys("16");
 		multiplyButton.click();
+		driver.switchTo().frame("multiplyResult");
 
+		WebElement multiplyOutput = driver.findElement(By.tagName("pre"));
+		String multiplyFrameText = multiplyOutput.getText();
+		System.out.println("Multiply frame prints: " + multiplyFrameText);
+		driver.switchTo().defaultContent();
+		
+		// input elements for mod numbers
 		WebElement modInput1 = driver.findElement(By.id("modNum1"));
 		WebElement modInput2 = driver.findElement(By.id("modNum2"));
 		WebElement modButton = driver.findElement(By.id("modButton"));
-
-		modInput1.sendKeys("12");
+		
+		modInput1.sendKeys("1583");
 		modInput2.sendKeys("7");
 		modButton.click();
+		driver.switchTo().frame("modResult");
+
+		WebElement modOutput = driver.findElement(By.tagName("pre"));
+		String frameText = modOutput.getText();
+		System.out.println("Mod frame prints: " + frameText);
+		driver.switchTo().defaultContent();
+		
+		// closing the driver
+		//driver.close();
 
 	}
 
